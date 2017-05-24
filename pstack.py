37d05853621a138c44485bf7044627fddf1f2759
sankalp58 <sankalp@parallelstack.com>
2017-05-25 00:53:09 +0530

    class methdo added for graphcics company '

--- pstack.py
+++ pstack.py
@@ -21,11 +21,16 @@ class Circle(object):
         'Performa quadrature on a shape of uniform radius '
         return  math.pi *self.radius**2.0
 
-
+    #for rubber sheet comapny
     def perimeter(self):
         'perform bprder length of curve for any given radius '
         return  math.pi * self.radius * 2.0
 
+    @classmethod
+    def from_bbd(cls,bbd):      #alternative construcotr
+        'Construct a circle from bounding box diagnal '
+        radius = bbd/2.0/math.sqrt(2.0)
+        return  Circle(radius)
 
 
 if __name__ == '__main__':
